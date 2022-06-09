29f5b17b970bdfbb98a256d58f6798b7c569a45d
kineticvenom <kalebvarnes@yahoo.com>
2022-06-09 10:04:36 -1000

    Python Solutions

--- python/recursion_spec.py
+++ python/recursion_spec.py
@@ -2,18 +2,18 @@ import math
 from recursion_challenge import factorial, palindrome, bottles, roman_num
 
 #Factorial test
-# for i in range(20):
-#     print(factorial(i)==math.factorial(i))
+for i in range(20):
+    print(factorial(i)==math.factorial(i))
 
 ##Palindrome test
-# print(palindrome("racecar") == True)
-# print(palindrome("meat taem") == True)
-# print(palindrome("apple") == False)
+print(palindrome("racecar") == True)
+print(palindrome("meat taem") == True)
+print(palindrome("apple") == False)
 
 ## Bottles Test
-# bottles(10)
+bottles(10)
 
-## Roman Numeral test
-# print(roman_num(24) == 'XXIV')
-# print(roman_num(110) == 'CX')
-# print(roman_num(90) == 'XC')
+# Roman Numeral test
+print(roman_num(24) == 'XXIV')
+print(roman_num(110) == 'CX')
+print(roman_num(90) == 'XC')
