16673ecb610bace68507e5c81e5f792e937b5c13
kineticvenom <kalebvarnes@yahoo.com>
2022-06-09 09:47:15 -1000

    Python solution

--- python/recursion_challenge.py
+++ python/recursion_challenge.py
@@ -1,11 +1,55 @@
-def factorial(x):
-	pass
+from string import ascii_letters, digits
 
+
+
+def factorial(x):
+	#base case 
+	if x == 0:
+		return 1
+	return x * factorial(x-1)
+
+valid_characters = set(ascii_letters + digits)
 def palindrome(string):
-	pass
+
+	if len(string) <= 1:
+		return True
+	first = 0
+	last = len(string) -1
+	while string[first] not in valid_characters:
+		first +=1
+	while string[last] not in valid_characters:
+		last -=1
+	if first <= last and string[first] == string[last]:
+		return palindrome(string[first+1:last])
+	return False
 
 def bottles(num):
-	pass
+	if not num:
+		print("No more bottles of beer on the wall, no more bottles of beer.")
+		print("Go to the store and buy some more, 99 bottles of beer on the wall...")
+		return
+	print(f"{num} {'bottle' if num == 1 else 'bottles'} of beer on the wall, {num} {'bottle' if num == 1 else 'bottles'} of beer.\nTake one down and pass it around, {num - 1} {'bottle' if num-1 == 1 else 'bottles'} of beer on the wall.")
+	bottles(num-1)
+		
 
-def roman_num(num):
-	pass
+romans_obj = {
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'XC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'IX': 9,
+        'V': 5,
+        'IV': 4,
+        'I': 1,
+    }
+def roman_num(num,key=list(romans_obj.keys())):
+	if not key:
+		return ''
+	count, num = divmod(num, romans_obj[key[0]])
+	return key[0] * count + roman_num(num,key[1:])
+
--- python/recursion_spec.py
+++ python/recursion_spec.py
@@ -1,1 +1,19 @@
-# Write your unit tests here
+import math
+from recursion_challenge import factorial, palindrome, bottles, roman_num
+
+#Factorial test
+# for i in range(20):
+#     print(factorial(i)==math.factorial(i))
+
+##Palindrome test
+# print(palindrome("racecar") == True)
+# print(palindrome("meat taem") == True)
+# print(palindrome("apple") == False)
+
+## Bottles Test
+# bottles(10)
+
+## Roman Numeral test
+# print(roman_num(24) == 'XXIV')
+# print(roman_num(110) == 'CX')
+# print(roman_num(90) == 'XC')
